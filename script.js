02958d0919648d7095e2fda1ef142dd70546df9f
overschool2 <btechologics@mail.ru>
2024-11-25 18:45:42 +0300

    commit

--- script.js
+++ script.js
@@ -1,24 +1,37 @@
+gsap.registerPlugin(ScrollTrigger);
 
-        gsap.registerPlugin(ScrollTrigger);
+gsap.to(".circle", {
+  scrollTrigger: {
+    trigger: ".line",
+    start: "top center", // Начало анимации, когда верх линии будет в центре экрана
+    end: "bottom center", // Конец анимации, когда низ линии будет в центре экрана
+    scrub: true, // Анимация синхронизирована со скроллом
+  },
+  y: 300, // Движение вправо на 400px
+  duration: 15,
+});
+gsap.to(".circle2", {
+  scrollTrigger: {
+    trigger: ".line2",
+    start: "top center", // Начало анимации, когда верх линии будет в центре экрана
+    end: "bottom center", // Конец анимации, когда низ линии будет в центре экрана
+    scrub: true, // Анимация синхронизирована со скроллом
+  },
+  y: 340, // Движение вправо на 400px
+  duration: 15,
+});
 
-        gsap.to(".circle", {
-            scrollTrigger: {
-                trigger: ".line",
-                start: "top center", // Начало анимации, когда верх линии будет в центре экрана
-                end: "bottom center", // Конец анимации, когда низ линии будет в центре экрана
-                scrub: true, // Анимация синхронизирована со скроллом
-            },
-            y:300, // Движение вправо на 400px
-            duration: 15,
-        });
-        gsap.to(".circle2", {
-            scrollTrigger: {
-                trigger: ".line2",
-                start: "top center", // Начало анимации, когда верх линии будет в центре экрана
-                end: "bottom center", // Конец анимации, когда низ линии будет в центре экрана
-                scrub: true, // Анимация синхронизирована со скроллом
-            },
-            y:340, // Движение вправо на 400px
-            duration: 15,
-        });
- 
+
+
+
+
+
+const root = document.documentElement;
+const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
+const marqueeContent = document.querySelector("ul.marquee-content");
+
+root.style.setProperty("--marquee-elements", marqueeContent.children.length);
+
+for(let i=0; i<marqueeElementsDisplayed; i++) {
+  marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+}
